614d836473cd13ab40b5bb8284494059b8163730
Melek Badreddine <mbadreddine5@gmail.com>
2024-10-07 14:29:11 +0100

    added firebase configuration

---
+++ frontend/src/app/environment.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCnvGKYixqDOGDGvqvv1ewktk9etLiZak8',
+  authDomain: 'spring-boot-microservice-53594.firebaseapp.com',
+  projectId: 'spring-boot-microservice-53594',
+  storageBucket: 'spring-boot-microservice-53594.appspot.com',
+  messagingSenderId: '976320271300',
+  appId: '1:976320271300:web:31ffd1b9e00c5234fd4a91',
+};
